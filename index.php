8d811bfdc4ce032d91a91f9c67b0e5545edcc316
JD2995 <JD92995@gmail.com>
2015-05-27 22:48:48 -0600

    Convertir y guardar imagen base64
    Funciones para guardar imágenes base64 en el directorio. Falta almacenar
    la dirección de la imagen en la base de datos

--- index.php
+++ index.php
@@ -3,6 +3,22 @@
 	<title>Consultas</title>
 <body>
 	<h1>Proyecto 2 Bases de datos</h1>
+	<?php
+	$servername = "localhost";
+	$username = "PROGRA_2";
+	$password = "progra_2";
 
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	echo "Connected successfully";
+	include_once("funciones.php");
+	guardarImagen("hola.jpg","prueba");
+	?>
+	<img src="Imagenes/prueba.php">
 	
 </html>
